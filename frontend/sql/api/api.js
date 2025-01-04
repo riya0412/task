44b95eb81f88353170bfa392e68535b1b7d8481f
Riya <117560822+riya0412@users.noreply.github.com>
2025-01-04 14:27:06 +0530

    Update api.js

--- frontend/sql/api/api.js
+++ frontend/sql/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080"; 
+const API_BASE_URL = "https://task-management-system-r1wt.onrender.com/"; 
 
 
 export const getUserData = (token) => {
